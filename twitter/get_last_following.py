10db6cf6ff1519b0ab5a4034f07544a7e03969cb
VictorCyprien <victor.cyprien@limayrac.fr>
2024-09-04 18:41:39 +0200

    Remove unused get_last_following_from_user function

--- twitter/get_last_following.py
+++ twitter/get_last_following.py
@@ -11,22 +11,6 @@ from json_helpers import extract_users_data, filter_empty_data
 logger = Logger()
 
 
-async def get_last_following_from_user(page: Page, user: str):
-    await page.goto(f"https://x.com/{user}/following")
-    await asyncio.sleep(3)
-    html = await page.inner_html("main[role=main]")
-    soup = BeautifulSoup(html, "html.parser")
-    last_following = soup.find("div", {"style": "transform: translateY(0px); position: absolute; width: 100%;"})
-    if "Suivre" in last_following.text:
-        print(f"{user} : {last_following.text.split("Suivre")[0]}")
-        last_following = last_following.text.split("Suivre")[0]
-    else:
-        print(f"{user} : {last_following.text.split("Abonné")[0]}")
-        last_following = last_following.text.split("Abonné")[0]
-    await asyncio.sleep(2)
-    return last_following
-
-
 async def get_last_followings_from_user(user_id: int) -> List[Dict]:
     cookies = open_json("cookies.json")
     cookies_dict = {}
