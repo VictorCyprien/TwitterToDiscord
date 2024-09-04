ef9838a13361bf74ee3da3de0f2d239e676c3ad6
VictorCyprien <victor.cyprien@limayrac.fr>
2024-09-04 16:00:44 +0200

    Improve scrapping with GraphQL API + Add methods for filters and extracts data more easily

--- twitter/__init__.py
+++ twitter/__init__.py
@@ -1,3 +1,3 @@
 from .login import connect
 from .create_driver import create_driver
-from .get_last_following import get_last_following_from_user
+from .get_last_following import get_last_following_from_user, get_last_followings_from_user
--- twitter/get_last_following.py
+++ twitter/get_last_following.py
@@ -1,7 +1,13 @@
 import asyncio
+import requests
 from bs4 import BeautifulSoup
 
 from playwright.async_api import Page
+
+from helpers import open_json, get_env_config, save_json, Logger
+from json_helpers import extract_users_data, filter_empty_data
+
+logger = Logger()
 
 
 async def get_last_following_from_user(page: Page, user: str):
@@ -18,3 +24,29 @@ async def get_last_following_from_user(page: Page, user: str):
         last_following = last_following.text.split("Abonné")[0]
     await asyncio.sleep(2)
     return last_following
+
+
+async def get_last_followings_from_user(page: Page):
+    cookies = open_json("cookies.json")
+    cookies_dict = {}
+    for one_cookie in cookies:
+        cookies_dict[one_cookie["name"]] = one_cookie["value"]
+
+    env = get_env_config()
+
+    headers = {
+        "Authorization": f"Bearer {env("TWITTER_BEARER_AUTH")}",
+        "x-csrf-token": cookies_dict["ct0"]
+    }
+
+    user_id = 3304410599
+    url = f"https://x.com/i/api/graphql/0yD6Eiv23DKXRDU9VxlG2A/Following?variables=%7B%22userId%22%3A%22{user_id}%22%2C%22count%22%3A50%2C%22includePromotedContent%22%3Afalse%7D&features=%7B%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22c9s_tweet_anatomy_moderator_badge_enabled%22%3Atrue%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22responsive_web_twitter_article_tweet_consumption_enabled%22%3Afalse%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Atrue%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Atrue%2C%22rweb_video_timestamps_enabled%22%3Atrue%2C%22longform_notetweets_rich_text_read_enabled%22%3Atrue%2C%22longform_notetweets_inline_media_enabled%22%3Atrue%2C%22responsive_web_media_download_video_enabled%22%3Afalse%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D"
+
+    res = requests.get(url=url, cookies=cookies_dict, headers=headers)
+    data = res.json()
+
+    save_json("data.json", data)
+    logger.info("Data saved !")
+
+    filtred_data = filter_empty_data(data)
+    extract_users_data(filtred_data)
