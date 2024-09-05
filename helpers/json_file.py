4ef57e051a00c95881be7ea5eebebf7f469e0887
VictorCyprien <victor.cyprien@limayrac.fr>
2024-09-05 19:20:22 +0200

    Add optional return type

--- helpers/json_file.py
+++ helpers/json_file.py
@@ -2,7 +2,7 @@ from typing import List, Dict
 import json
 from json import JSONDecodeError
 
-def open_json(filename: str) -> List[Dict]:
+def open_json(filename: str) -> List[Dict] | Dict:
     try:
         with open(filename, 'r') as file:
             data = json.load(file)
