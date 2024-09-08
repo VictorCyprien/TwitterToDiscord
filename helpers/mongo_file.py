36fd8c3f8fc660b85cc91770383b39b544395ec5
VictorCyprien <victor.cyprien@limayrac.fr>
2024-09-08 15:08:17 +0200

    Update MongoDBManager class

--- helpers/mongo_file.py
+++ helpers/mongo_file.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List
 
 from pymongo import MongoClient
 from pymongo.collection import Collection
@@ -8,21 +8,30 @@ from pymongo.cursor import Cursor
 
 class MongoDBManager():
     def __init__(self, url: str, db_name: str) -> None:
-        self.client = MongoClient(url)
+        self.client = MongoClient(url, serverSelectionTimeoutMS=5000)
         self.database = self.client[db_name]
         self.collection = None
+
+    def ping(self):
+        self.client.server_info()
     
     def set_collection(self, collection_name: str) -> Collection:
         self.collection = self.database[collection_name]
     
-    def get_all_data_from_collection(self) -> Cursor:
-        return self.collection.find()
+    def get_all_data_from_collection(self) -> List[Dict]:
+        return list(self.collection.find())
 
     def get_one_data_from_collection(self, filter: Dict):
         return self.collection.find(filter)
+    
+    def add_data_to_collection(self, data: Dict):
+        self.collection.insert_one(data)
 
     def update_one_data_from_collection(self, filter: Dict, update_values: Dict):
         self.collection.find_one_and_update(filter, update_values)
 
     def remove_one_data_from_collection(self, filter: Dict):
         self.collection.find_one_and_delete(filter)
+
+    def drop_data_from_collection(self):
+        self.collection.delete_many({})
